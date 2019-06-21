ccd00f6dc0a2ab25e00805c5085e8483c1ca85f6
even-gits <even@pop-os.localdomain>
2019-06-21 20:44:58 +0200

    *New: __init__.py with MY_STEAM_ID pull from system environment *New: steam_credentials folder storage location (also contains cm_servers.json)

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from os import environ as env
+from os import path
+
+from steam import SteamID
+
+try:
+    MY_STEAM_ID = SteamID(int(env['MY_STEAM_ID']))
+except Exception:
+    MY_STEAM_ID = None
+
+STEAM_CREDENTIAL_STORAGE = path.join(path.dirname(path.realpath(__file__)), 'steam_credentials')
